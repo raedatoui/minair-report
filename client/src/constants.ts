b5b1e6c53edc1f10c9fc3f70b21c0aabb0063f33
minair-affair <83786415+minair-affair@users.noreply.github.com>
2021-11-27 10:35:37 -0500

    rasp client (#3)

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,4 +1,4 @@
-export const CDN = 'https://storage.googleapis.com/api-project-992432653598.appspot.com';
+export const CDN = 'https://minair.me';
 export const fans = '/fans/';
 export const audio = '/audio/';
 export const menu = '/menu/';
